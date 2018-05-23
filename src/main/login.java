af1343324a5b8b3f55710017d5597ca9bd3b644a
JL <JL@192.168.1.37>
2018-05-23 21:04:30 +0200

    login nuevo

--- src/main/login.java
+++ src/main/login.java
@@ -3,27 +3,16 @@ package main;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-<<<<<<< HEAD
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JPanel;
-=======
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JLabel;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.Font;
->>>>>>> branch 'master' of https://github.com/damjuanlu/FutbolData.git
 
 public class login {
 
-	private JFrame frmFutboldata;
-	private JTextField txtUsuario;
+	private JFrame frame;
 	private JTextField txtPassword;
+	private JTextField txtUser;
+	private JButton btnLogin;
 
 	/**
 	 * Launch the application.
@@ -33,7 +22,7 @@ public class login {
 			public void run() {
 				try {
 					login window = new login();
-					window.frmFutboldata.setVisible(true);
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -52,90 +41,32 @@ public class login {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-<<<<<<< HEAD
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
-		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 262, Short.MAX_VALUE)
-		);
-		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 434, Short.MAX_VALUE)
-		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 262, Short.MAX_VALUE)
-		);
-		panel.setLayout(gl_panel);
-		frame.getContentPane().setLayout(groupLayout);
-=======
-		frmFutboldata = new JFrame();
-		frmFutboldata.getContentPane().setFont(new Font("Rockwell", Font.BOLD, 11));
-		frmFutboldata.setTitle("FUTBOLDATA");
-		frmFutboldata.setBounds(100, 100, 450, 300);
-		frmFutboldata.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel lblUsuario = new JLabel("USUARIO");
+		lblUsuario.setBounds(184, 34, 46, 14);
+		frame.getContentPane().add(lblUsuario);
 		
-		txtUsuario = new JTextField();
-		txtUsuario.setColumns(10);
+		JLabel lblContrasea = new JLabel("CONTRASE\u00D1A");
+		lblContrasea.setBounds(173, 101, 68, 14);
+		frame.getContentPane().add(lblContrasea);
 		
 		txtPassword = new JTextField();
+		txtPassword.setBounds(165, 126, 86, 20);
+		frame.getContentPane().add(txtPassword);
 		txtPassword.setColumns(10);
 		
-		JButton btnLog = new JButton("LOGIN");
+		txtUser = new JTextField();
+		txtUser.setColumns(10);
+		txtUser.setBounds(165, 59, 86, 20);
+		frame.getContentPane().add(txtUser);
 		
-		JLabel lblNewLabel = new JLabel("USUARIO");
-		
-		JLabel lblPassword = new JLabel("CONTRASE\u00D1A");
-		GroupLayout groupLayout = new GroupLayout(frmFutboldata.getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(166)
-							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-								.addComponent(txtPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(txtUsuario, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-									.addGap(10)
-									.addComponent(lblPassword))))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(183)
-							.addComponent(lblNewLabel))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(178)
-							.addComponent(btnLog)))
-					.addContainerGap(182, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(39)
-					.addComponent(lblNewLabel)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(txtUsuario, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(lblPassword)
-					.addGap(7)
-					.addComponent(txtPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(btnLog)
-					.addContainerGap(89, Short.MAX_VALUE))
-		);
-		frmFutboldata.getContentPane().setLayout(groupLayout);
-		
-		JPanel panel = new JPanel();
-
->>>>>>> branch 'master' of https://github.com/damjuanlu/FutbolData.git
+		btnLogin = new JButton("LOGIN");
+		btnLogin.setBounds(165, 169, 89, 23);
+		frame.getContentPane().add(btnLogin);
 	}
+
 }
