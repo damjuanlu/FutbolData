e0d5835a2d1e2f68637ca93158f42b663dac78e4
rballesterosa <rballesterosa@rballesterosa10.indra.es>
2018-05-24 16:28:39 +0200

    .

--- src/view/VistaLogin.java
+++ src/view/VistaLogin.java
@@ -1,6 +1,6 @@
 package view;
 
-import java.awt.EventQueue;
+import java.awt.*;
 import java.awt.Frame;
 
 import javax.swing.JFrame;
@@ -14,11 +14,13 @@ import javax.swing.JPasswordField;
 import java.awt.Toolkit;
 import javax.swing.ImageIcon;
 import java.awt.Window.Type;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Dimension;
 
-public class VistaLogin extends Frame{
+public class VistaLogin extends JFrame{
 
-	public JFrame frmFutboldata;
+	public JFrame frmSportClubData;
 	public JTextField txtUser;
 	public JButton btnLogin;
 	public JPasswordField txtPassword;
@@ -31,7 +33,7 @@ public class VistaLogin extends Frame{
 			public void run() {
 				try {
 					VistaLogin window = new VistaLogin();
-					window.frmFutboldata.setVisible(true);
+					window.frmSportClubData.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -50,46 +52,51 @@ public class VistaLogin extends Frame{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmFutboldata = new JFrame();
-		frmFutboldata.setResizable(false);
-		frmFutboldata.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\rballesterosa\\git\\FutbolData\\Repositorios\\icon-soccer\\026-strategy-1.png"));
-		frmFutboldata.setTitle("SportClubData");
-		frmFutboldata.getContentPane().setBackground(new Color(255, 255, 204));
-		frmFutboldata.setBounds(100, 100, 400, 250);
-		frmFutboldata.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmFutboldata.getContentPane().setLayout(null);
+		frmSportClubData = new JFrame();
+		frmSportClubData.setResizable(false);
+		frmSportClubData.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\rballesterosa\\git\\FutbolData\\Repositorios\\icon-soccer\\026-strategy-1.png"));
+		frmSportClubData.setTitle("SportClubData");
+		frmSportClubData.getContentPane().setBackground(new Color(255, 255, 204));
+		frmSportClubData.setBounds(100, 100, 400, 250);
+		frmSportClubData.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmSportClubData.getContentPane().setLayout(null);
 		
 		JLabel lblUsuario = new JLabel("USUARIO");
 		lblUsuario.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUsuario.setFont(new Font("Palatino Linotype", Font.BOLD, 11));
 		lblUsuario.setBounds(127, 35, 140, 14);
-		frmFutboldata.getContentPane().add(lblUsuario);
+		frmSportClubData.getContentPane().add(lblUsuario);
 		
 		JLabel lblContrasea = new JLabel("CONTRASE\u00D1A");
 		lblContrasea.setHorizontalAlignment(SwingConstants.CENTER);
 		lblContrasea.setFont(new Font("Palatino Linotype", Font.BOLD, 11));
 		lblContrasea.setBounds(127, 95, 140, 14);
-		frmFutboldata.getContentPane().add(lblContrasea);
+		frmSportClubData.getContentPane().add(lblContrasea);
 		
 		txtUser = new JTextField();
 		txtUser.setFont(new Font("Palatino Linotype", Font.BOLD, 11));
 		txtUser.setColumns(10);
 		txtUser.setBounds(127, 55, 140, 20);
-		frmFutboldata.getContentPane().add(txtUser);
+		frmSportClubData.getContentPane().add(txtUser);
 		
 		btnLogin = new JButton("LOGIN");
 		btnLogin.setFont(new Font("Palatino Linotype", Font.BOLD, 11));
 		btnLogin.setBounds(127, 169, 140, 23);
-		frmFutboldata.getContentPane().add(btnLogin);
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		frmSportClubData.getContentPane().add(btnLogin);
 		
 		txtPassword = new JPasswordField();
 		txtPassword.setBounds(127, 115, 140, 20);
-		frmFutboldata.getContentPane().add(txtPassword);
+		frmSportClubData.getContentPane().add(txtPassword);
 		
 		JLabel lblVersion = new JLabel("0.9.4");
 		lblVersion.setFont(new Font("Palatino Linotype", Font.PLAIN, 11));
 		lblVersion.setForeground(Color.LIGHT_GRAY);
 		lblVersion.setBounds(358, 207, 36, 14);
-		frmFutboldata.getContentPane().add(lblVersion);
+		frmSportClubData.getContentPane().add(lblVersion);
 	}
 }
