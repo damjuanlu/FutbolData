c37e79386075565a10ab41566d873088b1cd0be4
rballesterosa <rballesterosa@rballesterosa10.indra.es>
2018-05-28 12:52:10 +0200

    .

---
+++ src/view/VistaBarraMenu.java
@@ -0,0 +1,166 @@
+package view;
+
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextField;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+public class VistaBarraMenu extends JFrame{
+
+	public JPanel panel;
+	public JMenuBar menuPrincipal;
+	public JMenu mnInicio;
+	public JMenu mnJugadores;
+	public JMenuItem mntmFichas;
+	public JMenuItem mntmAsistencia;
+	public JMenuItem mntmEstadisticas;
+	public JMenu mnCompeticion;
+	public JMenuItem mntmMantenimientoEquipos;
+	public JMenuItem mntmPartidosDisputados;
+	public JMenuItem mntmConvocatorias;
+	public JMenuItem mntmCalendariosYResultados;
+	public JMenu mnEntrenamiento;
+	public JMenuItem mntmPlanificacionSesiones;
+	public JMenuItem mntmBaseDeDatos;
+	public JMenu mnAdministracion;
+	public JMenuItem mntmCuotasJugadores;
+	public JMenu mnAyuda;
+	public JMenuItem mntmUsuario;
+	public JMenuItem mntmContacto;
+	public JMenuItem mntmAcercaDe;
+	public JButton btnCerrarSesion;
+
+	public VistaBarraMenu() {
+		
+		int ancho=1080;
+		
+		panel = new JPanel();
+		
+		super.setContentPane(panel);
+		
+		//SE INSERTA LA BARRA DE MENÚ
+		menuPrincipal = new JMenuBar();
+		menuPrincipal.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		menuPrincipal.setBounds(0, 0, ancho, 21);
+		panel.add(menuPrincipal);
+		
+		mnInicio = new JMenu("INICIO");
+		mnInicio.setHorizontalAlignment(SwingConstants.CENTER);
+		mnInicio.setMargin(new Insets(2, 10, 0, 10));
+		mnInicio.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		menuPrincipal.add(mnInicio);
+		
+		mnJugadores = new JMenu("JUGADORES");
+		mnJugadores.setHorizontalAlignment(SwingConstants.CENTER);
+		mnJugadores.setMargin(new Insets(2, 10, 0, 10));
+		mnJugadores.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		menuPrincipal.add(mnJugadores);
+		
+		mntmFichas = new JMenuItem("FICHAS");
+		mntmFichas.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnJugadores.add(mntmFichas);
+		
+		mntmAsistencia = new JMenuItem("ASISTENCIA");
+		mntmAsistencia.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnJugadores.add(mntmAsistencia);
+		
+		mntmEstadisticas = new JMenuItem("ESTAD\u00CDSTICAS");
+		mntmEstadisticas.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnJugadores.add(mntmEstadisticas);
+		
+		mnCompeticion = new JMenu("COMPETICI\u00D3N");
+		mnCompeticion.setHorizontalAlignment(SwingConstants.CENTER);
+		mnCompeticion.setMargin(new Insets(2, 10, 0, 10));
+		mnCompeticion.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		menuPrincipal.add(mnCompeticion);
+		
+		mntmMantenimientoEquipos = new JMenuItem("MANTENIMIENTO EQUIPOS");
+		mntmMantenimientoEquipos.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnCompeticion.add(mntmMantenimientoEquipos);
+		
+		mntmPartidosDisputados = new JMenuItem("PARTIDOS DISPUTADOS");
+		mntmPartidosDisputados.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnCompeticion.add(mntmPartidosDisputados);
+		
+		mntmConvocatorias = new JMenuItem("CONVOCATORIAS");
+		mntmConvocatorias.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnCompeticion.add(mntmConvocatorias);
+		
+		mntmCalendariosYResultados = new JMenuItem("CALENDARIOS Y RESULTADOS");
+		mntmCalendariosYResultados.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnCompeticion.add(mntmCalendariosYResultados);
+		
+		mnEntrenamiento = new JMenu("ENTRENAMIENTO");
+		mnEntrenamiento.setHorizontalAlignment(SwingConstants.CENTER);
+		mnEntrenamiento.setMargin(new Insets(2, 10, 0, 10));
+		mnEntrenamiento.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		menuPrincipal.add(mnEntrenamiento);
+		
+		mntmPlanificacionSesiones = new JMenuItem("PLANIFICACI\u00D3N SESIONES");
+		mntmPlanificacionSesiones.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnEntrenamiento.add(mntmPlanificacionSesiones);
+		
+		mntmBaseDeDatos = new JMenuItem("BASE DE DATOS EJERCICIOS");
+		mntmBaseDeDatos.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnEntrenamiento.add(mntmBaseDeDatos);
+		
+		mnAdministracion = new JMenu("ADMINISTRACI\u00D3N");
+		mnAdministracion.setHorizontalAlignment(SwingConstants.CENTER);
+		mnAdministracion.setMargin(new Insets(2, 10, 0, 10));
+		mnAdministracion.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		menuPrincipal.add(mnAdministracion);
+		
+		mntmCuotasJugadores = new JMenuItem("CUOTAS JUGADORES");
+		mntmCuotasJugadores.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnAdministracion.add(mntmCuotasJugadores);
+		
+		mnAyuda = new JMenu("AYUDA");
+		mnAyuda.setHorizontalAlignment(SwingConstants.CENTER);
+		mnAyuda.setMargin(new Insets(2, 10, 0, 10));
+		mnAyuda.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		menuPrincipal.add(mnAyuda);
+		
+		mntmUsuario = new JMenuItem("USUARIO");
+		mntmUsuario.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnAyuda.add(mntmUsuario);
+		
+		mntmContacto = new JMenuItem("CONTACTO");
+		mntmContacto.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnAyuda.add(mntmContacto);
+		
+		mntmAcercaDe = new JMenuItem("ACERCA DE ...");
+		mntmAcercaDe.setFont(new Font("Palatino Linotype", Font.BOLD, 12));
+		mnAyuda.add(mntmAcercaDe);
+		
+		btnCerrarSesion = new JButton("CERRAR SESI\u00D3N");
+		menuPrincipal.add(btnCerrarSesion);
+		btnCerrarSesion.setFont(new Font("Palatino Linotype", Font.BOLD, 11));
+		
+		//ACTION LISTENER
+		
+		mntmFichas.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		btnCerrarSesion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+	}
+}
